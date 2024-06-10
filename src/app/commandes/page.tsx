8ac7b44b97a12a7553be2fda4d9a721b08959a4e
jldagniaux <jldagniaux@gmail.com>
2024-06-10 02:59:27 +0200

    add pages(commandes & livraison)

---
+++ src/app/commandes/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/system";
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Image, Link } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import Header from "../components/Header/header";
+import { Commande } from "../Interfaces/commande";
+import { Item } from "../Interfaces/item";
+import React from "react";
+
+const dividerStyle = {
+    width: '1px',
+    backgroundColor: 'black',
+    height: '100%',
+    margin: '0 8px'
+};
+
+const CommandesPage = () => {
+    const [commandesList, setCommandesList] = useState<Commande[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/commandes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ customer_id: 1 })
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setCommandesList(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+    return (
+        <NextUIProvider>
+            <Header />
+            {commandesList.map((commande: Commande) => (
+                <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '16px' }} key={commande.id_order}>
+                    <Card className="max-w-[60%]" style={{ flex: '1 1 calc(33.333% - 16px)', boxSizing: 'border-box' }}>
+                        <CardBody style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                            {commande.payment.time_payment && (
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <p>
+                                        {new Date(commande.payment.time_payment).toLocaleDateString('fr-FR', {
+                                            day: '2-digit',
+                                            month: '2-digit',
+                                            year: 'numeric'
+                                        })}
+                                    </p>
+                                    <div style={dividerStyle}></div>
+                                </div>
+                            )}
+
+                            {commande.items.map((item: Item, index: number) => (
+                                <React.Fragment key={item.id}>
+                                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                                        <p>
+                                            {item.name}
+                                        </p>
+                                        <div style={dividerStyle}></div>
+                                    </div>
+                                </React.Fragment>
+                            ))}
+
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <p>
+                                    {commande.price} €
+                                </p>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </div>
+            ))}
+        </NextUIProvider >
+    )
+}
+
+export default CommandesPage;
---
+++ src/app/livraison/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/system";
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Image, Link } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import Header from "../components/Header/header";
+import { Commande } from "../Interfaces/commande";
+import { Item } from "../Interfaces/item";
+import React from "react";
+
+const dividerStyle = {
+    width: '1px',
+    backgroundColor: 'black',
+    height: '100%',
+    margin: '0 8px'
+};
+
+const LivraisonPage = () => {
+    const [livraison, setLivraison] = useState<Commande>();
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/livraison', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ customer_id: 1 })
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setLivraison(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+    return (
+        <NextUIProvider>
+            <Header />
+            {livraison ? (
+                <Card className="max-w-[60%]" style={{ flex: '1 1 calc(33.333% - 16px)', boxSizing: 'border-box' }}>
+                    <CardBody style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                        {livraison.payment && livraison.payment.time_payment && (
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <p>
+                                    {new Date(livraison.payment.time_payment).toLocaleDateString('fr-FR', {
+                                        day: '2-digit',
+                                        month: '2-digit',
+                                        year: 'numeric'
+                                    })}
+                                </p>
+                                <div style={dividerStyle}></div>
+                            </div>
+                        )}
+
+                        {livraison.items.map((item: Item, index: number) => (
+                            <React.Fragment key={index}>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <p>
+                                        {item.name}
+                                    </p>
+                                    <div style={dividerStyle}></div>
+                                </div>
+                            </React.Fragment>
+                        ))}
+
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <p>
+                                {livraison.price} €
+                            </p>
+                        </div>
+                    </CardBody>
+                </Card>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </NextUIProvider>
+    )
+}
+
+export default LivraisonPage;
